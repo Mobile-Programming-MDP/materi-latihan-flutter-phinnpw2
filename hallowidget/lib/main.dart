7d0ffbf45aaa71538594f6743cb835a5d5d4122e
MalvinHendrawan <malvinhendrawan_2226250038@mhs.mdp.ac.id>
2023-10-13 18:25:42 +0700

    PIsah Widget

--- hallowidget/lib/main.dart
+++ hallowidget/lib/main.dart
@@ -3,21 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hallo Widget",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Malvin Hendrawan"),
-        ),
-        body: const Center(
-          child: Text("Hello Malvin Hendrawan"),
-        ),
+        title: "Hallo Widget",
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: FirstScreen());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Malvin Hendrawan"),
+      ),
+      body: const Center(
+        child: Text("Hello Malvin Hendrawan"),
       ),
     );
   }
